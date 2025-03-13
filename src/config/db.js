7dd2ddeb97a182df2362451747d811d87a9e6df0
Saimon Rocha <saimon.rocha07@gmail.com>
2025-03-13 14:04:31 -0300

    ajustando arquivos

--- src/config/db.js
+++ src/config/db.js
@@ -1,27 +1,20 @@
 require('dotenv').config();
 const { Sequelize } = require("sequelize");
 
-// Verifica se a variável de ambiente está carregada corretamente
+// Verifica se a variável de ambiente está definida corretamente
 if (!process.env.DATABASE_URL) {
   throw new Error("A variável DATABASE_URL não está definida!");
 }
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres', // Garante que o Sequelize está interpretando o banco corretamente
+  dialect: 'postgres', 
   dialectOptions: {
     ssl: {
-      require: true,  // Railway geralmente exige SSL para conexão com o banco
-      rejectUnauthorized: false, // Isso evita erro de certificado inválido
+      require: true,
+      rejectUnauthorized: false, 
     }
   },
-  logging: false, // Desativar logs no terminal
+  logging: false,
 });
-
-sequelize.authenticate()
-  .then(() => console.log("Conexão com o banco de dados foi estabelecida com sucesso!"))
-  .catch(err => {
-    console.error("Erro ao conectar com o banco de dados:", err);
-    process.exit(1); // Encerra o processo se a conexão falhar
-  });
 
 module.exports = sequelize;
--- src/database/index.js
+++ src/database/index.js
@@ -1,20 +1,11 @@
-const { Sequelize } = require('sequelize');
-const dbConfig = require('../config/db'); // Caminho para o arquivo db.js
-
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-  database_url: dbConfig.database_url,
-  host: dbConfig.host,
-  port: dbConfig.port,
-  dialect: dbConfig.dialect,
-  dialectOptions: dbConfig.dialectOptions
-});
+const sequelize = require('../config/db'); // Importando a instância do Sequelize já configurada
 
 sequelize.authenticate()
   .then(() => {
     console.log('Conexão com o banco de dados estabelecida com sucesso.');
   })
   .catch(err => {
-    console.error('Erro ao conectar ao banco de dados:', err);
+    console.error('Erro ao conectar com o banco de dados:', err);
   });
 
 module.exports = sequelize;
